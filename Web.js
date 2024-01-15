f87658fcfc2313da7e0e2120918c1163abb44f68
PolXavier <105446699+PolXavier@users.noreply.github.com>
2024-01-15 18:44:35 +0100

    Update Web.js

--- Web.js
+++ Web.js
@@ -47,11 +47,11 @@ class TheClass extends React.Component  {
               <br />
             <div className="Mainest_main">
         <h1>TevaDada</h1>      
-            <h2>Benvingut a, TevaDada! El teu servei personalitzat d'anàlisi de dades</h2>
+            <h2>Benvingut/da a, TevaDada! El teu servei personalitzat d'anàlisi de dades</h2>
 			<h4>Em dic Pol i m'acabo de graduar de la carrera de bioinformàtica. Durant aquests 3 anys he adquirit habilitats analítiques i és per això que he decidit emprendre i oferir les meves capacitats a empreses que busquen recolectar dades, analitzar-les, extreure resultats i conclusions.</h4>
 			<h4>Els llenguatges de programació (R, Python i Bash) i la preparació d'informes són els meus punts més forts.</h4>
                 	<p>Vols veure alguns dels meus treballs i projectes que he dut a terme durant la meva carrera professional? Clica <a target="_blank" href="https://github.com/PolXavier?tab=repositories">aquí.</a> Et portarà als meus repositoris que tinc al GitHub.</p>
-		        <h3 onClick={() => this.toggleSection('bioinformatics')}>Bioinformàtica</h3>
+		        <h3 onClick={() => this.toggleSection('bioinformatics')}>Bioinformàtica (clica a sobre per a desplegar més informació</h3>
 			<div id="bioinformatics" class="hidden-section">
 			<p>La bioinformàtica és una subdisciplina científica que implica l'ús de la informàtica per recopilar, emmagatzemar, analitzar i difondre dades i informació biològica (seqüències d'ADN, aminoàcids o anotacions sobre aquestes seqüències).</p>
 			<p>La bioinformàtica també abarca moltes més àrees, com per exemple, l'aprenentatge estatístic on es posen pràctica models de predicció, la visualització i base de dades, o, l'estudi de la genètica de poblacions. Algunes aplicacions i objectius són:</p>
@@ -63,7 +63,7 @@ class TheClass extends React.Component  {
 				<p>La meva especialització recau en l'analisi d'ADN ja que he treballat amb seqüències de material genètic mitocondrial. Si busques a algú experimentat en l'analisi estatístic poblacional, no dubtis en contactar-me!</p>
 				<img src="bioinfo.png" />
 						</div>
-			<h3 onClick={() => this.toggleSection('data-collection')}>Recollida de dades/anàlisi/informes/consultoria/assessoria</h3>
+			<h3 onClick={() => this.toggleSection('data-collection')}>Recollida de dades/anàlisi/informes/consultoria/assessoria (clica a sobre per a desplegar més informació)</h3>
 			<div id="data-collection" class="hidden-section">
 				<p>Els meus interessos van més allà de la biologia, i són molt més diversos, com per exemple, el món de l'esport. Estic disposat a ampliar els meus coneixements treballant per altres tipus d'empreses i aprendre noves coses! Si ets una empresa que busca un perfil com el meu no dubtis en contactar-me!</p>
 				<img src="futbol.png" />
