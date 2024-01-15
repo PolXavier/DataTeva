7b5d34437cd2a4ec1456820a7bef3be54b1ea4d3
PolXavier <105446699+PolXavier@users.noreply.github.com>
2024-01-15 12:25:29 +0100

    Update Web.js

--- Web.js
+++ Web.js
@@ -12,6 +12,11 @@ class TheClass extends Component {
     });
   }
 
+  toggleSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    section.classList.toggle('hidden-section');
+  }
+
   renderContent() {
     switch (this.state.page) {
       case "PersProj":
@@ -20,38 +25,18 @@ class TheClass extends Component {
             {/* Contenido para la página "Projectes" */}
             <h1>Projectes</h1>
             {/* ... Otro contenido ... */}
-		    <h2>Servei personalitzat d'anàlisi de dades</h2>
-		        <h4>Em dic Pol i m'acabo de graduar en la carrera de bioinformàtica. Durant aquests 3 anys he adquirit habilitats analítiques i és per això que he decidit emprendre i oferir les meves capacitats a empreses que busquen recolectar dades, analitzar-les, extreure resultats i conclusions.</h4>
-		        <h4>Els llenguatges de programació (R, Python i Bash) i la preparació d'informes són els meus punts més forts.</h4>
-		
-		        <p>Vols veure alguns dels meus treballs i projectes que he dut a terme durant la meva carrera professional? Clica <a target="_blank" href="https://github.com/PolXavier?tab=repositories">aquí.</a> Et portarà als meus repositoris que tinc al GitHub.</p>
-		
-		        <h3 onclick="toggleSection('bioinformatics')">Bioinformàtica</h3>
-		        <div id="bioinformatics" class="hidden-section">
-		            <p>La bioinformàtica és una subdisciplina científica que implica l'ús de la informàtica per recopilar, emmagatzemar, analitzar i difondre dades i informació biològica (seqüències d'ADN, aminoàcids o anotacions sobre aquestes seqüències).</p>
-		            <p>La bioinformàtica també abarca moltes més àrees, com per exemple, l'aprenentatge estatístic on es posen pràctica models de predicció, la visualització i base de dades, o, l'estudi de la genètica de poblacions. Algunes aplicacions i objectius són:</p>
-		            <ul>
-		                <li>Augmentar la comprensió de la salut i de malalties.</li>
-		                <li>Aplicacions en l'agricultura per fer els cultius més resistents i eficients (agrogenòmica).</li>
-		                <li>Investigació per fer vacunes.</li>
-		            </ul>
-		            <p>La meva especialització recau en l'analisi d'ADN ja que he treballat amb seqüències de material genètic mitocondrial. Si busques a algú experimentat en l'analisi estatístic poblacional, no dubtis en contactar-me!</p>
-		            <img src="https://photos.fife.usercontent.google.com/pw/ABLVV86nQnJJ5qIVjEKPOdFlYDhryZrOUOioCqEN38doJVX1ZogdtshSkFZ4qw=w501-h291-s-no-gm?authuser=1">
-		        </div>
-		
-		        <h3 onclick="toggleSection('data-collection')">Recollida de dades/anàlisi/informes/consultoria/assessoria</h3>
-		        <div id="data-collection" class="hidden-section">
-		            <p>Els meus interessos van més allà de la biologia, i són molt més diversos, com per exemple, el món de l'esport. Estic disposat a ampliar els meus coneixements treballant per altres tipus d'empreses i aprendre noves coses! Si ets una empresa que busca un perfil com el meu no dubtis en contactar-me!</p>
-		            <img src="https://github.com/PolXavier/P-gina-web/blob/main/futbol.png">
-		        </div>
-		
-		        <footer>
-		            <p>Aquesta actuació està impulsada i subvencionada pel Departament d’Empresa i Treball i
-		            cofinançada per la Unió Europea mitjançant el Fons Social Europeu Plus.</p>
-		
-		            <img src="https://github.com/PolXavier/P-gina-web/blob/main/CoFinancatUE_lateral.png">
-		            <img src="https://github.com/PolXavier/P-gina-web/blob/main/Next%20Generation%2BGeneralitat.png">
-		        </footer>
+            <h2>Servei personalitzat d'anàlisi de dades</h2>
+            {/* ... Otro contenido ... */}
+            <h3 onClick={() => this.toggleSection('bioinformatics')}>Bioinformàtica</h3>
+            <div id="bioinformatics" className="hidden-section">
+              {/* Contenido de la sección */}
+            </div>
+            {/* ... Otro contenido ... */}
+            <h3 onClick={() => this.toggleSection('data-collection')}>Recollida de dades</h3>
+            <div id="data-collection" className="hidden-section">
+              {/* Contenido de la sección */}
+            </div>
+            {/* ... Otro contenido ... */}
           </div>
         );
       case "Inici":
