13e1c72a6c27725fb0e1a0c7322d66229f208ca0
PolXavier <105446699+PolXavier@users.noreply.github.com>
2024-01-15 13:23:50 +0100

    Update Web.js

--- Web.js
+++ Web.js
@@ -22,37 +22,26 @@ class TheClass extends React.Component  {
       case "PersProj":
         return (
           <div>
-            {/* Contenido para la página "Projectes" */}
             <h1>Projectes</h1>
-            {/* ... Otro contenido ... */}
-            
-            {/* ... Otro contenido ... */}
           </div>
         );
       case "Inici":
         return (
           <div>
-            {/* Contenido para la página "Inici" */}
             <h1>Inici</h1>
             <h2>Servei personalitzat d'anàlisi de dades</h2>
-            {/* ... Otro contenido ... */}
             <h3 onClick={() => this.toggleSection('bioinformatics')}>Bioinformàtica</h3>
             <div id="bioinformatics" className="hidden-section">
-              {/* Contenido de la sección */}
             </div>
-            {/* ... Otro contenido ... */}
             <h3 onClick={() => this.toggleSection('data-collection')}>Recollida de dades</h3>
             <div id="data-collection" className="hidden-section">
-              {/* Contenido de la sección */}
             </div>
           </div>
         );
       case "AcProjects":
         return (
           <div>
-            {/* Contenido para la página "Academic Projects" */}
             <h1>Academic Projects</h1>
-            {/* ... Otro contenido ... */}
           </div>
         );
       default:
